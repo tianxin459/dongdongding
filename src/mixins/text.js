5588289b05db849eab7b7a70c64fa01a3310c257
Ellis Tian <e0t00nk@WMSC.wal-mart.com>
2019-11-01 15:06:55 +0800

    office handover 11-1-2019

--- src/mixins/text.js
+++ src/mixins/text.js
@@ -45,12 +45,12 @@ export default class text extends wepy.mixin {
                         TransDesc2: '发送弹幕/聊天信息',
                         TransDesc3: '收款二维码',
                         ChatRoom: '聊天室',
-                        ChatRoomUrl: 'http://ellist.cn/bulletmessage/bullet.html',
+                        ChatRoomUrl: 'http://ellist.cn/wmtan/bullet.html',
                         ChatRoomDesc: '打开以上链接主屏幕，聊天信息同时会作为弹幕发送到主屏幕',
                         Album: '活动相册',
                         AlbumDesc: '活动相册，可以上传相片与他人分享',
                         RunningGame: '摇一摇赛跑',
-                        RunningGameUrl: 'http://ellist.cn/bulletmessage/race.html',
+                        RunningGameUrl: 'http://ellist.cn/wmtan/race.html',
                         RunningGameDesc: '打开以上链接赛跑页面，然后手机上点击“开始”开始赛跑，参与者可以在摇一摇赛跑中点击“准备”参加赛跑',
                         Deposit: '充值',
                         DepositDesc: '摇一摇手机充值，点击“开始”开始摇手机充值，5秒内摇的次数即为充值金额，倒数结束后可以重试，但是您只有一次机会点击充值，充值确认后该菜单将关闭'
@@ -62,7 +62,7 @@ export default class text extends wepy.mixin {
                     Desc1: '按下按钮开始比赛',
                     WaitSignal: '等待比赛开始',
                     ClickReady: '点击就位按钮参加比赛',
-                    Url: '电脑上打开http://ellist.cn/bulletmessage/race.html'
+                    Url: '电脑上打开http://ellist.cn/wmtan/race.html'
                 }
             },
             {
@@ -95,12 +95,12 @@ export default class text extends wepy.mixin {
                         TransDesc2: 'Send live chat to big screen/chat room',
                         TransDesc3: 'Your QR code to recieve P2P transfer',
                         ChatRoom: 'Chat Room',
-                        ChatRoomUrl: 'http://ellist.cn/bulletmessage/bullet.html',
+                        ChatRoomUrl: 'http://ellist.cn/wmtan/bullet.html',
                         ChatRoomDesc: 'Send messages or photos to chat with others. The messages will also be sent to the big screen as 弹幕.',
                         Album: 'Album',
                         AlbumDesc: 'Send annual party photos to share with others.',
                         RunningGame: 'Running Game',
-                        RunningGameUrl: 'http://ellist.cn/bulletmessage/race.html',
+                        RunningGameUrl: 'http://ellist.cn/wmtan/race.html',
                         RunningGameDesc: 'Participate in the lucky draw during the dinner party. More details to be shared after dinner begins.',
                         Deposit: 'Deposit Money',
                         DepositDesc: 'Shake you cell phone within 5 seconds to deposit to your balance. You can try unlimited times to go for as much as possible. But you can only choose to deposit the money for one time.'
@@ -112,7 +112,7 @@ export default class text extends wepy.mixin {
                     Desc1: 'click button to start race',
                     WaitSignal: 'Wait for Start Signal...',
                     ClickReady: 'Click Ready Button',
-                    Url: 'open http://ellist.cn/bulletmessage/race.html'
+                    Url: 'open http://ellist.cn/wmtan/race.html'
                 }
             }
         ]
